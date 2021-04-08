a057d29008d3e873bf7e9a9ad3a1729c185ac46a
ErinCain <ecain@flowwest.com>
2021-04-08 08:27:35 -0700

    Corrects strata to look at sacramento river

--- frequency_exceeding_temps/make_frequency_exceedence_csv.R
+++ frequency_exceeding_temps/make_frequency_exceedence_csv.R
@@ -5,7 +5,7 @@ library(tidyverse)
 secchi_temp <- read_csv("delta_water_quality_data_with_strata.csv") 
 filtered_temp <- secchi_temp %>% 
   mutate(water_year = ifelse(Month >= 8, Year + 1, Year)) %>% 
-  filter(Region == c("Yolo Bypass", "South Delta")) %>% glimpse() 
+  filter(Region == c("Yolo Bypass", "Sacramento River")) %>% glimpse() 
 
 load("~/Git/delta-secchi-temperature-data/frequency_exceeding_temps/water_year_indices.rda") 
 
@@ -18,10 +18,12 @@ temp_with_wy <- filtered_temp %>%
   left_join(water_years) %>%
   group_by(water_year, water_year_type, Region) %>%
   summarise(march_above_15 = sum(Month == 3 & Temperature > 15)/sum(Month == 3), 
-            april_above_17 = sum(Month == 4 & Temperature > 17)/sum(Month == 4)) %>%
+            april_above_17 = sum(Month == 4 & Temperature > 17)/sum(Month == 4)) %>% 
   select(water_year, water_year_type, march_above_15, april_above_17, strata = Region) %>% na.omit()
 
+View(temp_with_wy)
 #write_csv(temp_with_wy, "frequency_exceeding_temps/cache_slough_temp_exceeding_thresholds.csv")
-write_csv(temp_with_wy, "frequency_exceeding_temps/cache_slough_strata_temp_exceeding_thresholds.csv")
+#write_csv(temp_with_wy, "frequency_exceeding_temps/cache_slough_strata_temp_exceeding_thresholds.csv")
+
 # Checks a few months to see if proportion exceeding match up 
 # explore_temp <- filtered_temp %>% filter(Year == 2008, Month == 4) %>% mutate(above_17 = sum(Temperature > 17)/n())
